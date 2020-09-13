8fe1a0f52ced58144ee8547db7f44d5c72419a29
Bagus P <maspamzdev@gmail.com>
2020-09-13 11:46:37 +0700

    Update config.php

--- week3/kamus/application/config/config.php
+++ week3/kamus/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/udacoding/week3/kamus/ci3/';
+$config['base_url'] = 'http://localhost/kamus/';
 
 /*
 |--------------------------------------------------------------------------
